f7ec52306acc0dc2f00a54bdfc0eddea35ed788e
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-07 16:03:48 +0300

    calculate commit

--- app/src/main/java/com/example/sensietiptime/MainActivity.kt
+++ app/src/main/java/com/example/sensietiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity(){
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +20,22 @@ class MainActivity : AppCompatActivity(){
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val cost = stringInTextField.toDoubleOrNull()
 
-        val tipPercentage =when(selectedId){
+        val tipPercentage =when(binding.tipOptions.checkedRadioButtonId){
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+        if (cost == null || cost == 0.0){
+            
+            binding.tipResult.text = ""
+            return
+        }
         var  tip = tipPercentage*cost
         val roundUp = binding.roundUpSwitch.isChecked
         if (roundUp){
-            tip = ceil(tip)
+            ceil(tip).also { tip = it }
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
