bb0f818af079cef374d32aa3aa227e5ab2facb03
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-09 10:23:30 +0300

    themes and styles commit

--- app/src/main/java/com/example/sensietiptime/MainActivity.kt
+++ app/src/main/java/com/example/sensietiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sensietiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.sensietiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity(){
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view,keyCode) }
     }
 
     private fun calculateTip() {
@@ -39,7 +45,14 @@ class MainActivity : AppCompatActivity(){
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
-
-
+    }
+    fun handleKeyEvent(view: View,keyCode:Int): Boolean{
+        if (keyCode== KeyEvent.KEYCODE_ENTER){
+//            Hide the keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+            return true
+        }
+        return false
     }
 }
